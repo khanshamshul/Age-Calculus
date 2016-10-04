f632c30331472be2551aba63d82e264d5b0fe54f
PRIYES BAMNE <priyes@users.noreply.github.com>
2016-10-04 10:26:43 -0700

    Age-Calculus Version 1.1
    Age-Calculus Version 1.1 calculates age of a person (USER) in Day (DD),Month(MM) and Year (YY) Format which was not possible/included in previous version (Age-Calulus Version 1.0).  #FOSS #PRACTICAL #S.I.W.S #PRIYES

--- AGE CALCULUS Ver 1.0.py
+++ AGE CALCULUS Ver 1.0.py
@@ -1,16 +1,66 @@
-###Age_Calculus is a free program written in python for FYCS Practical
-
+###Age_Calculus is a free program written python
+##This is Age Calculus version 1.1 by Priyes Bamne
+##This versions also calculates Month and date which is not available in previous Age -Calculus V1.0
+import datetime
 import math
-##Take input from user
+priyes = datetime.datetime.now()
+##Getting input from use and displaying error if date/month/year are invalid
+def AgeCalculus(month, day, year):
 
-a=int(input('ENTER CURRENT YEAR (YYYY) =')) 
-b=int(input('ENTER YOUR DATE OF BIRTH (YYYY) ='))
-c= a-b
+    if (user_month > 12 or user_month < 1):
+        print("Enter a valid month")
+    
+    elif (priyes.month < user_month):
 
-##Error for invalid year
-if b>a:
-       print('ENTER VALID DATE OF BIRTH')
-else:
-       print('YOUR CURRENT AGE IS =',c)
-       
+        month = 12 + (priyes.month - user_month)
+        year = priyes.year - user_year - 1
+
+        if (priyes.day <= user_day):
+            day = user_day - priyes.day
+
+        elif (priyes.day >= user_day):
+            day = priyes.day - user_day
+
+        else:
+            print("Enter a valid date")
+
+    elif(priyes.month >= user_month): 
+
+        if (priyes.month == user_month):
+
+            if priyes.day >= user_day:
+                month = 0
+                year = priyes.year - user_year
+
+            else:
+                month = 11
+                year = priyes.year - user_year - 1
+
+        else:
+            month = priyes.month - user_month
+            year = priyes.year - user_year
+
+        if (user_day > 31 or user_day < 1):
+            print("Enter a valid date")
+
+        elif (priyes.day < user_day):
+            day = 30 - (user_day - priyes.day)
+
+        elif (priyes.day >= user_day):
+            day = priyes.day - user_day
+
+    print("Your current age is",year," years,",month," month and",day," days.")         
+               
+
+user_month = int(input("Enter the month: "))
+user_day = int(input("Enter the day: "))
+user_year = int(input("Enter the year: "))
+
+AgeCalculus(0, 0, 0)
+
+
+##FOSS PRACTICAL 
+##This is an open project
+##Do contribute
+
        
